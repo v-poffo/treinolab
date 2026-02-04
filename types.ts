15f1d9d015f76ebe213769b9bae8b1c807d73a8f
v-poffo <vinipoffo@gmail.com>
2026-02-03 23:21:59 -0300

    Add files via upload

--- types.ts
+++ types.ts
@@ -10,7 +10,7 @@ export interface Exercise {
 }
 
 export interface WorkoutDay {
-  category: string;
+  category: string; // 'A', 'B', 'C', 'D'
   title: string;
   exercises: Exercise[];
 }
@@ -57,7 +57,6 @@ export interface UserProfile {
   targetWeight: string;
   trainingFrequency: number;
   trainingGoal: string;
-  groupId?: string; // NOVO: Para agrupar amigos
   weights: WeightEntry[];
   cycle: WorkoutCycle | null;
   cardio: CardioSession | null;
